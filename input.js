6d6a9fb86049f1a392d425cb49ac082d9f359457
Jeremy Dombrowski <dev@jeremyfelix.com>
2020-11-04 16:09:09 -0800

    Move input functions to input.js

---
+++ input.js
@@ -0,0 +1,24 @@
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+
+  // on any input from stdin (standard input)
+  stdin.on("data", handleUserInput);
+
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  // \u0003 maps to ctrl+c input
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+module.exports = { setupInput, handleUserInput };
--- play.js
+++ play.js
@@ -1,28 +1,7 @@
 // play.js
 const { connect } = require("./client");
-/**
- * Setup User Interface
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-
-  // on any input from stdin (standard input)
-  stdin.on("data", handleUserInput);
-
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  // \u0003 maps to ctrl+c input
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
+const input = require("./input");
 
 console.log("Connecting ...");
-setupInput();
+input.setupInput();
 connect();
